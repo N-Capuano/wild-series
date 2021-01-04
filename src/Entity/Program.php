4a1eeee9ceb18c5d5d181722e8ed009ded769e7f
n-capuano <nicolas.capuano.dev@gmail.com>
2021-01-04 21:50:56 +0100

    quest 13 finished

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -5,10 +5,17 @@ namespace App\Entity;
 use App\Repository\ProgramRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProgramRepository")
+ * @Assert\EnableAutoMapping()
+ * @UniqueEntity(
+ *     fields={"title"},
+ *     errorPath="title",
+ *     message="Ce titre existe déjà")
  */
 class Program
 {
@@ -21,11 +28,25 @@ class Program
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Regex(
+     *     pattern="/^plus belle la vie$/i",
+     *     match=false,
+     *     message="On parle de vraies séries ici"
+     * )
+
+     * @Assert\Length(
+     *  min = 2,
+     *  max = 255,
+     *  minMessage = "Your first name must be at least {{ limit }} characters long",
+     *  maxMessage = "Your first name cannot be longer than {{ limit }} characters"
+     * )
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank
      */
     private $summary;
 
